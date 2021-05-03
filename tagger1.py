339e46a075c9fba545c2a5f6e482911093c41128
Nadav Spitzer <nspitzern@gmail.com>
2021-05-03 15:14:30 +0300

    fixed part1.pdf graphs

--- tagger1.py
+++ tagger1.py
@@ -1,9 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-import matplotlib.pyplot as plt
-from torch.optim.lr_scheduler import ExponentialLR, StepLR
 
 from utils import save_model, draw_graphs
 
